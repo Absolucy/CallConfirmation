d684f2d31d7b61facbcfb8488eda91a012642f55
Lucy <lucy@absolucy.moe>
2022-01-31 10:49:01 -0500

    Update to work on latest discord

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
+import {getModule} from "aliucord-api/dist/modules/module";
 import {getUser} from "aliucord-api/dist/modules/users";
-import {User} from "aliucord-api/dist/types/users";
+import {showDialog} from "aliucord-api/dist/modules/dialog";
 
-const computeChannelName = getModule(
-	(m) => m.computeChannelName
-).computeChannelName;
 var callModule = getModule(
 	(m) => m.default?.call && m.default?.ring && m.default?.stopRinging
 );
+
 const _call = callModule.default.call;
 callModule.default.call = (id, unk1, isManaged, recipientId, onCall) => {
-	getUser(`${recipientId}`).then((user: User) => {
-		let show = getModule((m) => m.default?.show).default.show;
-		show({
+	getUser(`${recipientId}`).then((user) => {
+		showDialog({
 			title: "Call?",
 			body: `Are you sure you want to start a call with ${user.username}#${user.discriminator}?`,
 			confirmText: "Yes, start the call",
