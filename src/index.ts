8012d65d38f1b13a651dc612d0ef23476267940b
Lucy <lucy@absolucy.moe>
2022-01-31 13:06:26 -0500

    More improvements from Zoey (thanks!)

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,37 @@
 import {getModule} from "aliucord-api/dist/modules/module";
-import {getUser} from "aliucord-api/dist/modules/users";
 import {showDialog} from "aliucord-api/dist/modules/dialog";
+
+const channelModule = getModule((m) => m.default?.getChannel);
 
 var callModule = getModule(
 	(m) => m.default?.call && m.default?.ring && m.default?.stopRinging
 );
 
 const _call = callModule.default.call;
-callModule.default.call = (id, unk1, isManaged, recipientId, onCall) => {
-	getUser(`${recipientId}`).then((user) => {
-		showDialog({
-			title: "Call?",
-			body: `Are you sure you want to start a call with ${user.username}#${user.discriminator}?`,
-			confirmText: "Yes, start the call",
-			cancelText: "Nope, that was an accident",
-			onConfirm: function () {
-				_call(id, unk1, isManaged, recipientId, onCall);
-			},
-			onCancel: function () {},
-		});
+callModule.default.call = (...args) => {
+	const channelID = args[0];
+	const channel = channelModule.default.getChannel(channelID);
+	let name;
+
+	if (channel.type == 3) {
+		name = channel.name;
+
+		if (name.trim() === "") {
+			name = channel.rawRecipients.map((user) => user.username).join(", ");
+		}
+	} else {
+		const user = channel.rawRecipients[0];
+		name = `${user.username}#${user.discriminator}`;
+	}
+
+	showDialog({
+		title: "Call?",
+		body: `Are you sure you want to start a call with ${name}?`,
+		confirmText: "Yes, start the call",
+		cancelText: "Nope, that was an accident",
+		onConfirm: () => {
+			_call(args);
+		},
+		onCancel: () => {},
 	});
 };
