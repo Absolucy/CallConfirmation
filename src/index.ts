9f97e481d64b480b977036b991b84109b4c16278
Lucy <lucy@absolucy.moe>
2022-01-31 19:22:34 -0500

    Use new patching system

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {getModule} from "aliucord-api/dist/modules/module";
 import {showDialog} from "aliucord-api/dist/modules/dialog";
+import {patchInstead} from "aliucord-api/dist/modules/patcher";
 
 const channelModule = getModule((m) => m.default?.getChannel);
 
@@ -7,31 +8,35 @@ var callModule = getModule(
 	(m) => m.default?.call && m.default?.ring && m.default?.stopRinging
 );
 
-const _call = callModule.default.call;
-callModule.default.call = (...args) => {
-	const channelID = args[0];
-	const channel = channelModule.default.getChannel(channelID);
-	let name;
+patchInstead(
+	"CallConfirmation",
+	callModule.default,
+	"call",
+	(self, args, res) => {
+		const channelID = args[0];
+		const channel = channelModule.default.getChannel(channelID);
+		let name;
 
-	if (channel.type == 3) {
-		name = channel.name;
+		if (channel.type == 3) {
+			name = channel.name;
 
-		if (name.trim() === "") {
-			name = channel.rawRecipients.map((user) => user.username).join(", ");
+			if (name.trim() === "") {
+				name = channel.rawRecipients.map((user) => user.username).join(", ");
+			}
+		} else {
+			const user = channel.rawRecipients[0];
+			name = `${user.username}#${user.discriminator}`;
 		}
-	} else {
-		const user = channel.rawRecipients[0];
-		name = `${user.username}#${user.discriminator}`;
-	}
 
-	showDialog({
-		title: "Call?",
-		body: `Are you sure you want to start a call with ${name}?`,
-		confirmText: "Yes, start the call",
-		cancelText: "Nope, that was an accident",
-		onConfirm: () => {
-			_call(args);
-		},
-		onCancel: () => {},
-	});
-};
+		showDialog({
+			title: "Call?",
+			body: `Are you sure you want to start a call with ${name}?`,
+			confirmText: "Yes, start the call",
+			cancelText: "Nope, that was an accident",
+			onConfirm: () => {
+				res(args);
+			},
+			onCancel: () => {},
+		});
+	}
+);
